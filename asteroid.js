f4600f56011990d1b7e3f997803a428690836b30
zhenyezhao <86699069+zhenyezhao@users.noreply.github.com>
2023-11-19 18:35:33 -0800

    Add files via upload

---
+++ asteroid.js
@@ -0,0 +1,11 @@
+AFRAME.registercomponent('asteroid',{
+    init:function(){
+        for (var i=1; i<=20, i++){
+            //i
+            var id=`asteroid$(i)`;
+
+            //position variables
+            var posX=math.floor(math.random()=1000+ 1000)
+        }
+    }
+})
